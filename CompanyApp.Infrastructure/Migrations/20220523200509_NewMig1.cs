fe20fcd4901d3e1a1ca4c71fb817fd8ba9a1cd2a
I553132 <I553132@W-PF1MYMK5>
2022-05-24 01:37:57 +0500

    migration for new db

---
+++ CompanyApp.Infrastructure/Migrations/20220523200509_NewMig1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MainTitle",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Prediction = table.Column<string>(type: "text", nullable: false),
+                    Probability = table.Column<double>(type: "double precision", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MainTitle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsDateGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsDateGroups", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MainTitle");
+
+            migrationBuilder.DropTable(
+                name: "NewsDateGroups");
+        }
+    }
+}
---
+++ CompanyApp.Infrastructure/Migrations/20220523202325_NewMig2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "NewsDateGroupId",
+                table: "MainTitle",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MainTitle_NewsDateGroupId",
+                table: "MainTitle",
+                column: "NewsDateGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MainTitle_NewsDateGroups_NewsDateGroupId",
+                table: "MainTitle",
+                column: "NewsDateGroupId",
+                principalTable: "NewsDateGroups",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MainTitle_NewsDateGroups_NewsDateGroupId",
+                table: "MainTitle");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MainTitle_NewsDateGroupId",
+                table: "MainTitle");
+
+            migrationBuilder.DropColumn(
+                name: "NewsDateGroupId",
+                table: "MainTitle");
+        }
+    }
+}
